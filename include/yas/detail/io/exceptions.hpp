272e6852841cc9b787f1910cbfd7b84dd407e409
niXman <i.nixman@gmail.com>
2013-07-09 19:57:47 +0400

    copyright years updated

--- include/yas/detail/io/exceptions.hpp
+++ include/yas/detail/io/exceptions.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2012 niXman (i dot nixman dog gmail dot com)
+// Copyright (c) 2010-2013 niXman (i dot nixman dog gmail dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
